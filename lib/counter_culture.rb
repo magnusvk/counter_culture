8bcb6ee65f29f59e0888388bd8b3b089803a90e2
Magnus von Koeller <magnus@vonkoeller.de>
2013-04-03 13:38:00 -0400

    refactor

--- lib/counter_culture.rb
+++ lib/counter_culture.rb
@@ -113,7 +113,7 @@ module CounterCulture
                 }
                 # use update_all because it's faster and because a fixed counter-cache shouldn't
                 # update the timestamp
-                klass.update_all "#{column_name} = #{counts[model.id].to_i}", "id = #{klass.connection.quote(model.id)}"
+                klass.where(:id => model.id).update_all(column_name => counts[model.id])
               end
             end
           end
