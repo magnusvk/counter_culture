4f03c60fcf7eac78ccd0b575e3aabaef995b9bd1
Magnus von Koeller <hello@magnusvk.com>
2014-01-10 09:50:13 -0500

    make our test app work with Rails master

--- spec/rails_app/config/environments/production.rb
+++ spec/rails_app/config/environments/production.rb
@@ -60,8 +60,4 @@ RailsApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
