ecee056b3b3ebe7b59a78c9450e3d5b5086f671b
Swapnil Abnave <swap.it@hotmail.com>
2016-06-26 03:34:05 +0530

    'Prefer' spaces over Tab

--- spec/join_option_spec.rb
+++ spec/join_option_spec.rb
@@ -21,50 +21,50 @@ describe "CounterCulture" do
     let!(:conv_memb_bob)   {ConversationMember.create(conversation_id: conversation.id, member_id: bob.id)}
     let!(:conv_memb_guest) {ConversationMember.create(conversation_id: conversation.id, member_id: mr_guest.id)}
 
-	  describe "conditional counts" do
-	    it "should give me correct guest count" do
-	    	conversation.reload
-	    	conversation.guest_count.should == 1
-	  	end
+    describe "conditional counts" do
+      it "should give me correct guest count" do
+        conversation.reload
+        conversation.guest_count.should == 1
+      end
 
-	    it "should update the count on delete" do
-	    	conv_memb_guest.destroy
-	    	conversation.reload
+      it "should update the count on delete" do
+        conv_memb_guest.destroy
+        conversation.reload
 
-	    	conversation.guest_count.should == 0
-	  	end
-	  end
+        conversation.guest_count.should == 0
+      end
+    end
 
-	  describe "fix_counts" do
-	  	let!(:mr_x)      { Member.create(name: 'Mr. X', user_type: 'G') }
-	  	let!(:conv_mr_x) {ConversationMember.create(conversation_id: conversation.id, member_id: mr_x.id)}
+    describe "fix_counts" do
+      let!(:mr_x)      { Member.create(name: 'Mr. X', user_type: 'G') }
+      let!(:conv_mr_x) {ConversationMember.create(conversation_id: conversation.id, member_id: mr_x.id)}
 
-	  	it "should give me correct count for legacy data" do
-	  		conversation.update_column(:guest_count, 0)
-	  		conversation.reload
+      it "should give me correct count for legacy data" do
+        conversation.update_column(:guest_count, 0)
+        conversation.reload
 
-	  		conversation.guest_count.should == 0
+        conversation.guest_count.should == 0
 
-	  		ConversationMember.counter_culture_fix_counts
+        ConversationMember.counter_culture_fix_counts
 
-	  		conversation.reload
+        conversation.reload
 
-	  		conversation.guest_count.should == 3
-	  	end
+        conversation.guest_count.should == 3
+      end
 
-	  	it "should update of delete" do
-	  		ConversationMember.delete_all
+      it "should update of delete" do
+        ConversationMember.delete_all
 
-	  		conversation.reload
+        conversation.reload
 
-	  		conversation.guest_count.should_not eq(0)
+        conversation.guest_count.should_not eq(0)
 
-	  		ConversationMember.counter_culture_fix_counts
+        ConversationMember.counter_culture_fix_counts
 
-	  		conversation.reload
+        conversation.reload
 
-	  		conversation.guest_count.should == 0
-	  	end
-	  end
-	end
+        conversation.guest_count.should == 0
+      end
+    end
+  end
 end
