6d05af6431e3ad0e8d95f2302dac072d3af493eb
Magnus von Koeller <hello@magnusvk.com>
2017-06-12 10:28:04 -0400

    Rails 5.1 now released

--- test_rails_versions.sh
+++ test_rails_versions.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for RAILS_VERSION in "3.2.0" "4.0.0" "4.1.0" "4.2.0" "5.0.0" "5.1.0.rc1"
+for RAILS_VERSION in "3.2.0" "4.0.0" "4.1.0" "4.2.0" "5.0.0" "5.1.0"
 do
   export RAILS_VERSION
   echo "Rails $RAILS_VERSION"
