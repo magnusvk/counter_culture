31bc1cda1c6b447e6cf176cdfedae6af4040e7f1
Magnus von Koeller <hello@magnusvk.com>
2018-04-13 18:45:29 -0400

    Test against Rails 5.2

--- test_rails_versions.sh
+++ test_rails_versions.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for RAILS_VERSION in "~> 3.2.0" "~> 4.0.0" "~> 4.1.0" "~> 4.2.0" "~> 5.0.0" "= 5.1.4" "= 5.1.5" "~> 5.1.5"
+for RAILS_VERSION in "~> 3.2.0" "~> 4.0.0" "~> 4.1.0" "~> 4.2.0" "~> 5.0.0" "= 5.1.4" "= 5.1.5" "~> 5.1.5" "~> 5.2.0"
 do
   export RAILS_VERSION
   echo "Rails $RAILS_VERSION"
