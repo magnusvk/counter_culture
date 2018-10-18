f8f190265c9a864e822d8768d2c23501f633beff
Magnus von Koeller <hello@magnusvk.com>
2018-10-18 12:22:31 -0400

    I think I can remove this also

--- spec/models/with_module/model2.rb
+++ spec/models/with_module/model2.rb
@@ -2,7 +2,6 @@ module WithModule
   class Model2 < ActiveRecord::Base
     self.table_name_prefix = 'with_module_'
 
-    has_many :model1s, class_name: 'WithModule::Model1',
-      foreign_key: :with_module_model2_id
+    has_many :model1s
   end
 end
