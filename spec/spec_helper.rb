603409545b46dba0b5a674adc76168ec0e16cc17
Magnus von Koeller <hello@magnusvk.com>
2021-04-07 09:46:35 -0600

    Re-enable paper_trail tests with newest Rails and Ruby (#315)
    Re-enable paper_trail tests with newest Rails and Ruby
    
    ... now that they finally released version 12.0.0 with the required
    fix.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,6 @@ require 'rails/all'
 module PapertrailSupport
   def self.supported_here?
     return false if Gem::Version.new(Rails.version) < Gem::Version.new('5.0.0')
-    return false if Gem::Version.new(Rails.version) >= Gem::Version.new('6.1.0')
-    return false if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.0.0')
     true
   end
 end
@@ -23,6 +21,7 @@ require 'timecop'
 
 if PapertrailSupport.supported_here?
   require 'paper_trail'
+  require "paper_trail/frameworks/active_record"
   require 'paper_trail/frameworks/active_record/models/paper_trail/version'
   require 'paper_trail/frameworks/rspec'
 end
