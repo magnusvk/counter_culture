ad8ca9ba9d914b3ab94749317946df8066664cef
Magnus von Koeller <magnus@tremendous.com>
2025-03-20 08:58:39 -0600

    Tweak specs because we already test against different Rails versions

--- spec/counter_culture/configuration_spec.rb
+++ spec/counter_culture/configuration_spec.rb
@@ -6,40 +6,24 @@ RSpec.describe CounterCulture::Configuration do
   end
 
   describe '.configure' do
-    context 'when Rails version is >= 6.1' do
-      before do
-        allow_any_instance_of(CounterCulture::Configuration)
-          .to receive(:rails_supports_read_replica?)
-          .and_return(true)
-      end
-
-      it 'allows setting use_read_replica' do
+    it 'works according to the Rails version' do
+      if Gem::Requirement.new('>= 7.1.0').satisfied_by?(ActiveRecord.version)
         CounterCulture.configure do |config|
           config.use_read_replica = true
         end
 
         expect(CounterCulture.configuration.use_read_replica).to be true
-      end
-
-      it 'defaults to false' do
-        expect(CounterCulture.configuration.use_read_replica).to be false
-      end
-    end
-
-    context 'when Rails version is < 6.1' do
-      before do
-        allow_any_instance_of(CounterCulture::Configuration)
-          .to receive(:rails_supports_read_replica?)
-          .and_return(false)
-      end
-
-      it 'raises error when setting use_read_replica to true' do
+      else
         expect {
           CounterCulture.configure do |config|
             config.use_read_replica = true
           end
-        }.to raise_error("Counter Culture's read replica support requires Rails 6.1 or higher")
+        }.to raise_error("Counter Culture's read replica support requires Rails 7.1 or higher")
       end
+    end
+
+    it 'defaults to false' do
+      expect(CounterCulture.configuration.use_read_replica).to be false
     end
   end
 
