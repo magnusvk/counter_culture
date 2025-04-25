9350ea0af263c7ca1213f7c1912ea8ed1e0bc4bc
Madison Holtzer <maddieholtzer@gmail.com>
2025-04-25 13:23:19 -0400

    Showcase of a change that works on 3.9.0 but not 3.10.0

--- spec/models/conversation.rb
+++ spec/models/conversation.rb
@@ -1,4 +1,8 @@
 class Conversation < ActiveRecord::Base
   belongs_to :candidate
+  delegate(
+    :candidate_profile,
+    to: :candidate
+  )
   counter_culture [:candidate, :candidate_profile]
 end
