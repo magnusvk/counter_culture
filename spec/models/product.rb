6ebacc8d9b1e18b42076025145087e4a727f3de2
Magnus von Koeller <magnus@tremendous.com>
2025-07-10 10:33:04 -0600

    Fix regression for `foreign_key_values` by adding support for composite primary keys (#417)
    It turns out the old tests here weren't properly reloading and thus didn't catch this regression.

--- spec/models/product.rb
+++ spec/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ActiveRecord::Base
   belongs_to :category
 
-  counter_culture :category, :foreign_key_values => proc {|foreign_key_value| Category.pluck(:id) }
+  counter_culture :category, :foreign_key_values => lambda { |foreign_key_value|
+    if foreign_key_value.present?
+      Category.pluck(:id)
+    else
+      nil
+    end
+  }
 
   if PapertrailSupport.supported_here?
     has_paper_trail
